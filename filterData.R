66aad27258a464f62e89d8ea5ba65c04307723b7
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2025-01-06 11:09:59 -0300

    feat: filter

--- filterData.R
+++ filterData.R
@@ -20,7 +20,7 @@ filter_csv_Telefonia <- function(csv_to_filter, telefonia_csv, csv_name) {
   write.csv(Telefonia_XX_filtrado, file = csv_name, row.names=FALSE)
 }
 
-filter_csv_Acessos <- function(csv_to_filter, acessos_csv, csv_name) {
+filter_csv_Velocidade <- function(csv_to_filter, acessos_csv, csv_name) {
   Acessos_bruto <- read.csv(acessos_csv, sep=";", dec=",")
   
   Municipios <- read.csv(csv_to_filter, sep=";", dec=",")
@@ -29,6 +29,9 @@ filter_csv_Acessos <- function(csv_to_filter, acessos_csv, csv_name) {
   write.csv(Acessos_filtrados, file = csv_name, row.names=FALSE)
 }
 
-filter_csv_Acessos("Maiores_Municipios_Amunorpi_IBGE_OK.csv", "Velocidade_Contratada_SCM.csv", "top4_velocidade_contratada.csv")
+setwd("C:/Users/pedro/OneDrive/Área de Trabalho/ProbEstatistica")
+
+filter_csv_Velocidade("Maiores_Municipios_Amunorpi_IBGE_OK.csv", "Velocidade_Contratada_SCM.csv", "top4_velocidade_contratada.csv")
+filter_csv_Velocidade("Curitiba_IBGE_OK.csv", "Velocidade_Contratada_SCM.csv", "curitiba_velocidade_contratada.csv")
 
 
