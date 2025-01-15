7944f0dd39c56cdb733d5fd8c83a9d203d599007
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2025-01-15 08:52:49 -0300

    clean: limpando arquivos

--- filterData.R
+++ filterData.R
@@ -8,7 +8,7 @@ filter_csv_BandaLarga <- function(csv_to_filter, banda_larga_csv, csv_name) {
   
   BandaLargaFx_XX_filtrado <- merge(BandaLargaFx_XX_bruto, Municipios, by = "Código.IBGE.Município")
   BandaLargaFx_XX_filtrado <- BandaLargaFx_XX_filtrado %>% select(-"Grupo.Econômico", -"Empresa", -"CNPJ", -"Porte.da.Prestadora")
-  write.csv(BandaLargaFx_XX_filtrado, file = csv_name, row.names=FALSE, encoding = "UTF-8")
+  write_excel_csv(BandaLargaFx_XX_filtrado, file = csv_name)
 } 
 
 filter_csv_Telefonia <- function(csv_to_filter, telefonia_csv, csv_name) {
@@ -18,7 +18,7 @@ filter_csv_Telefonia <- function(csv_to_filter, telefonia_csv, csv_name) {
   
   Telefonia_XX_filtrado <- merge(Telefonia_XX_bruto, Municipios, by = "Código.IBGE.Município")
   Telefonia_XX_filtrado <- Telefonia_XX_filtrado %>% select(-"Ano", -"Mês", -"Tecnologia.Geração", -"Tipo.de.Pessoa", -"Tipo.de.Produto", -"Modalidade.de.Cobrança", -"Acessos")
-  write.csv(Telefonia_XX_filtrado, file = csv_name, row.names=FALSE, fileEncoding = "UTF-8")
+  write_excel_csv(Telefonia_XX_filtrado, file = csv_name)
 }
 
 filter_csv_Velocidade <- function(csv_to_filter, acessos_csv, csv_name) {
@@ -30,9 +30,9 @@ filter_csv_Velocidade <- function(csv_to_filter, acessos_csv, csv_name) {
   write_excel_csv(Acessos_filtrados, file = csv_name)
 }
 
-setwd("C:\\Users\\pedro\\OneDrive\\Área de Trabalho\\ProbEstatistica")
+for(ano in 2:4) {
+}
 
-
-filter_csv_Velocidade("Maiores_Municipios_Amunorpi_IBGE_OK.csv", "Velocidade_Contratada_SCM.csv", "top4_velocidade_contratada.csv")
-filter_csv_Velocidade("curitiba_IBGE_OK.csv", "Velocidade_Contratada_SCM.csv", "curitiba_velocidade_contratada.csv")
-filter_csv_Velocidade("Dados_IBGE_Amunorpi_OK.csv", "Velocidade_Contratada_SCM.csv", "amunorpi_velocidade_contratada.csv")
+#filter_csv_BandaLarga("Maiores_Municipios_Amunorpi_IBGE_OK.csv", "Velocidade_Contratada_SCM.csv", "top4_velocidade_contratada.csv")
+#filter_csv_BandaLarga("curitiba_IBGE_OK.csv", "Velocidade_Contratada_SCM.csv", "curitiba_velocidade_contratada.csv")
+#filter_csv_BandaLarga("Dados_IBGE_Amunorpi_OK.csv", "Velocidade_Contratada_SCM.csv", "amunorpi_velocidade_contratada.csv")
